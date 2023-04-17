2e77a69826aa5f3f7047ad9aef82d6a30724f498
Ayesha Vermani <ayeshavermani@gmail.com>
2023-04-17 16:07:04 +0100

    clean up

--- tests/vanderpol/exp01_vanderpol.py
+++ tests/vanderpol/exp01_vanderpol.py
@@ -1,3 +1,5 @@
+import argparse
+
 import torch
 import torch.nn.functional as F
 from torchvision import datasets, transforms
@@ -16,19 +18,52 @@ n_train = 300
 "load vanderpol data"
 data = torch.load('vanderpol.pt')
 
-"extract reference data"
-x, y = data['0']
-x_train, y_train = x[:, :n_train, :], y[:, :n_train, :]
 
-data_ref = DataLoader(DataSetTs(x_train.float(), y_train.float()), batch_size=80)
+def train_ref_vae():
+    "extract reference data"
+    x, y = data['0']
+    x_train, y_train = x[:, :n_train, :], y[:, :n_train, :]
 
-dy_ref = y.shape[2]
+    data_ref = DataLoader(DataSetTs(x_train.float(), y_train.float()), batch_size=80)
 
-prior = Prior(dx)
-vae = SeqVae(dx, dy_ref, dh)
-vae_tr, prior_tr = vae_training(vae, prior, 100, data_ref)
+    dy_ref = y.shape[2]
 
-i=0
+    prior = Prior(dx)
+    vae = SeqVae(dx, dy_ref, dh)
+    res = vae_training(vae, prior, 100, data_ref)
+
+    torch.save(res, 'reference_model.pt')
+
+
+def reuse_dynamics(reference, lstq):
+
+    vae = torch.load(reference)
+
+    for key in data:
+        res_alignment = obs_alignment(vae, data[key], lstq)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--train_prior', help='1 to specify if the vae needs to be trained from scratch')
+    parser.add_argument('-l', '--lstq', type=int, help='1 if lstq can be used for alignment')
+    parser.add_argument('f', '--path', type=str, help='if train_prior is set to 0, provide path of trained model')
+
+    args = parser.parse_args()
+
+    if args.train_prior == 1:
+        train_ref_vae()
+        fname = 'reference_model.pt'
+    else:
+        fname = args.path
+
+    reuse_dynamics(fname, bool(args.lstq))
+
+
+if __name__ == '__main__':
+    main()
+
+
 
 
 
