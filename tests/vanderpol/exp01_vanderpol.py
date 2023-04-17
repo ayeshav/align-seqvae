fe2289c042f43eec53710832b3dc4c2de61805e5
Ayesha Vermani <ayeshavermani@gmail.com>
2023-04-17 13:52:34 +0100

    add dataloader to train in batches

--- tests/vanderpol/exp01_vanderpol.py
+++ tests/vanderpol/exp01_vanderpol.py
@@ -11,16 +11,22 @@ import matplotlib.pyplot as plt
 dx = 2
 dh = 256
 
+n_train = 300
+
 "load vanderpol data"
 data = torch.load('vanderpol.pt')
 
 "extract reference data"
 x, y = data['0']
+x_train, y_train = x[:, :n_train, :], y[:, :n_train, :]
+
+data_ref = DataLoader(DataSetTs(x_train.float(), y_train.float()), batch_size=80)
+
 dy_ref = y.shape[2]
 
 prior = Prior(dx)
 vae = SeqVae(dx, dy_ref, dh)
-vae_tr = vae_training(vae, prior, 5, (x.float(), y.float()))
+vae_tr, prior_tr = vae_training(vae, prior, 100, data_ref)
 
 i=0
 
--- tests/vanderpol/utils.py
+++ tests/vanderpol/utils.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn as nn
 from torch.distributions import Normal
 from seq_vae import SeqVae
+from torch.utils.data import Dataset
 
 
 def compute_elbo(vae, prior, y):
@@ -19,8 +20,28 @@ def vae_training(vae, prior, epochs, data):
 
     opt = torch.optim.Adam(params=list(prior.parameters()) + list(vae.parameters()))
     for _ in range(epochs):
-        opt.zero_grad()
-        loss = compute_elbo(vae, prior, data[1])
-        loss.backward()
-        opt.step()
-    return vae
+        for x, y in data:
+            opt.zero_grad()
+            loss = compute_elbo(vae, prior, y.permute(1, 0, 2))
+            loss.backward()
+            opt.step()
+
+            with torch.no_grad():
+                print(loss.item())
+    return vae, prior
+
+
+class DataSetTs(Dataset):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+        self.len = y.shape[1]
+
+    def __getitem__(self, idx):
+        return self.x[:, idx, :], self.y[:, idx, :]
+
+    def __len__(self):
+        return self.len
+
+
