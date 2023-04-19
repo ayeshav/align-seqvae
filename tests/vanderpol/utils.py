3b1e8b3f55c24338e649932bd92c84ad56ca3767
Ayesha Vermani <ayeshavermani@gmail.com>
2023-04-19 16:09:40 +0100

    functions to do lstq or learn invertible mapping, on to debugging

--- tests/vanderpol/utils.py
+++ tests/vanderpol/utils.py
@@ -3,14 +3,14 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torch.distributions import Normal
-from seq_vae import SeqVae
+from seq_vae import SeqVae, Prior
 from torch.utils.data import Dataset
 
 
 def compute_elbo(vae, prior, y):
 
-    ll, kl = vae(y, prior)
-    elbo = torch.mean(ll - kl)
+    _, ll_params, kl = vae(y, prior)
+    elbo = torch.mean(ll_params[2] - kl)
 
     return -elbo
 
@@ -30,7 +30,53 @@ def vae_training(vae, prior, epochs, data):
     return vae, prior
 
 
-def obs_alignment(ref_res, prior, y, y_ref, lstq):
+def get_predictions(y_aligned, ref_vae, prior):
+    assert isinstance(prior, Prior)
+    assert isinstance(ref_vae, SeqVae)
+
+    encoder_params, likelihood_params, _ = ref_vae(y_aligned, prior)
+
+    x_samples = encoder_params[2]
+    y_recon = likelihood_params[0]
+
+
+def train_invertible_mapping(epochs, ref_vae, prior, y, y_ref, rp_mat):
+
+    dy = y_ref.shape[2]
+    linear_map = nn.Parameter(torch.ones(dy, dy), requires_grad=True)
+
+    opt = torch.optim.Adam(params=linear_map)
+    for _ in range(epochs):
+        opt.zero_grad()
+        loss = compute_map_mse(ref_vae, prior, linear_map, y, rp_mat)
+        loss.backward()
+        opt.step()
+
+        with torch.no_grad():
+            print(loss.item())
+
+    return linear_map
+
+
+def compute_map_mse(ref_vae, prior, linear_map, y, rp_mat):
+    assert isinstance(prior, Prior)
+    assert isinstance(ref_vae, SeqVae)
+
+    y_tfm = y@rp_mat@linear_map
+
+    encoder_params, likelihood_params, _ = ref_vae(y_tfm, prior)
+
+    y_tfm_recon = likelihood_params[0]
+
+    "now invert it back to original space"
+    y_tfm_recon_original = (y_tfm_recon@np.linalg.pinv(linear_map))@np.linalg.pinv(rp_mat)
+
+    mse = torch.mean((y - y_tfm_recon_original)**2)
+
+    return mse
+
+
+def obs_alignment(ref_res, prior, y, y_ref, lstq, epochs=20):
     """
     should return an alignment function (can be linear) that takes in y_new
     """
@@ -52,13 +98,12 @@ def obs_alignment(ref_res, prior, y, y_ref, lstq):
         A = u.dot(vh)
         y_cat_tfm = (y_cat @ A.T) * np.sum(s)
 
+        # TODO: Figure out how to avoid doing the pinv
+
         return (y_cat_tfm@np.linalg.pinv(rp_mat)).reshape(T, N, dy)
 
     else:
-
-        "after random projection, it can be passed through the regular vae"
-
-
+        linear_map = train_invertible_mapping(epochs, ref_res, prior, y, y_ref, rp_mat)
 
 
 class DataSetTs(Dataset):
@@ -75,3 +120,9 @@ class DataSetTs(Dataset):
         return self.len
 
 
+class Mlp(nn.Module):
+    def __init__(self, dx, dy):
+        self.linear = nn.Sequential(nn.Linear(dx,dy))
+
+    def forward(self, x):
+        return self.linear(x)
