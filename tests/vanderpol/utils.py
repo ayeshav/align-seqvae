c7c2a078eeaf5b820c526aa944fdcd1f3e2404b1
Josue <josue.nassar@stonybrook.edu>
2023-06-29 12:50:49 -0400

    Update training function and SeqDataLoader. Everything assumes that data is Batch by Time by Dimension (default setting for RNNs in torch)

--- tests/vanderpol/utils.py
+++ tests/vanderpol/utils.py
@@ -1,50 +1,75 @@
 import math
-
-import numpy as np
-
 import torch
 import torch.nn as nn
 from torch.distributions import Normal, MultivariateNormal
-from seq_vae import SeqVae, Prior
+from seq_vae import SeqVae
 from torch.utils.data import Dataset
 from tqdm import tqdm
 
 
-def compute_elbo(vae, prior, y):
+class SeqDataLoader:
+    def __init__(self, data_tuple, batch_size, shuffle=False):
+        """
+        Constructor for fast data loader
+        :param data_tuple: a tuple of matrices of size Batch size x Time x dy
+        :param batch_size: batch size
+        """
+        self.shuffle = shuffle
+        self.data_tuple = data_tuple
+        self.batch_size = batch_size
+        self.dataset_len = self.data_tuple[0].shape[0]
 
-    enc_params, ll_params, log_prior = vae(y, prior)
-    kl = enc_params[3] - log_prior
+        # Calculate # batches
+        n_batches, remainder = divmod(self.dataset_len, self.batch_size)
+        if remainder > 0:
+            n_batches += 1
+        self.n_batches = n_batches
 
-    elbo = torch.mean(ll_params[2] - kl)
+    def __iter__(self):
+        if self.shuffle:
+            r = torch.randperm(self.dataset_len)
+        else:
+            r = torch.arange(self.dataset_len)
 
-    return -elbo
+        self.indices = [r[j * self.batch_size: (j * self.batch_size) + self.batch_size] for j in range(self.n_batches)]
+        self.i = 0
+        return self
+
+    def __next__(self):
+        if self.i >= self.n_batches:
+            raise StopIteration
+        idx = self.indices[self.i]
+        batch = tuple([self.data_tuple[i][idx] for i in range(len(self.data_tuple))])
+        self.i += 1
+        return batch
+
+    def __len__(self):
+        return self.n_batches
 
 
-def vae_training(vae, prior, epochs, data):
-
-    opt = torch.optim.Adam(params=list(prior.parameters()) + list(vae.parameters()), lr=1e-2)
-    losses = []
-    for _ in tqdm(range(epochs)):
-        for x, y in data:
+def vae_training(vae, train_dataloader, n_epochs=100, lr=5e-4, weight_decay=1e-4):
+    """
+    function that will train a vae
+    :param vae: a SeqVae object
+    :param train_dataloader: a dataloader object
+    :param n_epochs: Number of epochs to train for
+    :param lr: learning rate of optimizer
+    :param weight_decay: value of weight decay
+    :return: trained vae and training losses
+    """
+    assert isinstance(vae, SeqVae)
+    opt = torch.optim.AdamW(params=vae.parameters(), lr=lr, weight_decay=weight_decay)
+    training_losses = []
+    for _ in tqdm(range(n_epochs)):
+        for y in train_dataloader:
             opt.zero_grad()
-            loss = compute_elbo(vae, prior, y.permute(1, 0, 2))
+            loss = vae(y.to(vae.device))
             loss.backward()
             opt.step()
 
             with torch.no_grad():
-                print(loss.item())
-                losses.append(loss.item())
-    return vae, prior, losses
-
-
-# def get_predictions(y_aligned, ref_vae, prior):
-#     assert isinstance(prior, Prior)
-#     assert isinstance(ref_vae, SeqVae)
-#
-#     encoder_params, likelihood_params, _ = ref_vae(y_aligned, prior)
-#
-#     x_samples = encoder_params[2]
-#     y_recon = likelihood_params[0]
+                training_losses.append(loss.item())
+    return vae, training_losses
 
 
 def compute_map_mse(ref_vae, prior, linear_map, y):
@@ -126,66 +151,3 @@ def obs_alignment(ref_res, prior, y, y_ref, epochs=20, update_prior=True):
     linear_map, prior = train_invertible_mapping(epochs, ref_res, prior, y, y_ref, rp_mat, update_prior)
     return linear_map, rp_mat, prior
 
-# Utils for data
-
-
-class SeqDataLoader:
-    def __init__(self, data_tuple, batch_size, shuffle=False):
-        """
-        Constructor for fast data loader
-        :param data_tuple: a tuple of matrices of size T x K x dy
-        :param batch_size: batch size
-        """
-        self.shuffle = shuffle
-        self.data_tuple = data_tuple
-        self.batch_size = batch_size
-        self.dataset_len = self.data_tuple[0].shape[1]
-
-        # Calculate # batches
-        n_batches, remainder = divmod(self.dataset_len, self.batch_size)
-        if remainder > 0:
-            n_batches += 1
-        self.n_batches = n_batches
-
-    def __iter__(self):
-        if self.shuffle:
-            r = torch.randperm(self.dataset_len)
-        else:
-            r = torch.arange(self.dataset_len)
-
-        self.indices = [r[j * self.batch_size: (j * self.batch_size) + self.batch_size] for j in range(self.n_batches)]
-        self.i = 0
-        return self
-
-    def __next__(self):
-        if self.i >= self.n_batches:
-            raise StopIteration
-        idx = self.indices[self.i]
-        batch = tuple([self.data_tuple[i][:, idx, :] for i in range(len(self.data_tuple))])
-        self.i += 1
-        return batch
-
-    def __len__(self):
-        return self.n_batches
-
-
-class DataSetTs(Dataset):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-        self.len = y.shape[1]
-
-    def __getitem__(self, idx):
-        return self.x[:, idx, :], self.y[:, idx, :]
-
-    def __len__(self):
-        return self.len
-
-
-# class Mlp(nn.Module):
-#     def __init__(self, dx, dy):
-#         self.linear = nn.Sequential(nn.Linear(dx,dy))
-#
-#     def forward(self, x):
-#         return self.linear(x)
