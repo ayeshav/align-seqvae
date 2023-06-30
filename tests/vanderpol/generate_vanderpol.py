e724a19ce45c61ba05a36a21986a174b7403c908
Josue <josue.nassar@stonybrook.edu>
2023-06-30 10:47:33 -0400

    numerically stable vanderpol

--- tests/vanderpol/generate_vanderpol.py
+++ tests/vanderpol/generate_vanderpol.py
@@ -3,6 +3,7 @@ import numpy.random as npr
 import torch
 import os
 from scipy.integrate import odeint
+import matplotlib.pyplot as plt
 
 torch.manual_seed(42)
 torch.random.manual_seed(42)
@@ -47,55 +48,98 @@ def noisy_vanderpol(T, t_eval, params, x0):
     return x
 
 
-"params for vdp"
+def noisy_vanderpol_v2(K, T, dy, sigma_x, sigma_y, mu=1.5, dt=1e-2):
+    x = np.empty((K, T, 2))
+    y = np.empty((K, T, dy))
+
+    # generate random readout
+    C = npr.randn(2, dy) / np.sqrt(2)
+
+    # generate initial conditions
+    x[:, 0] = 6 * npr.rand(K, 2) - 3
+    y[:, 0] = x[:, 0] @ C + sigma_y * npr.randn(K, dy)
+
+    # propagate time series
+    for t in range(1, T):
+        vel = np.empty((K, 2))
+        vel[:, 0] = mu * (x[:, t -1, 0] - x[:, t - 1, 0] ** 3 / 3 - x[:, t - 1, 1])
+        vel[:, 1] = x[:, t - 1, 0] / mu
+
+        x[:, t] = x[:, t - 1] + dt * (vel + sigma_x * npr.randn(K, 2))
+        y[:, t] = x[:, t] @ C + sigma_y * npr.randn(K, dy)
+    return x, y, C
+
+
+# "params for vdp"
+# mu = 1.5
+# dt = 1e-2
+#
+# "params dict for noisy vdp"
+# params = {}
+# params['mu'] = mu
+# params['tau_1'] = 0.1
+# params['tau_2'] = 0.1
+# params['sigma'] = 0.05  # noise add into euler integration
+# params['scale'] = 1 / 0.4
+
+sigma_x = 0.1  # state noise
+sigma_y = 0.1  # observation noise
+dt = 1e-2  # euler integration time step
 mu = 1.5
-dt = 1e-2
-
-"params dict for noisy vdp"
-params = {}
-params['mu'] = mu
-params['tau_1'] = 0.1
-params['tau_2'] = 0.1
-params['sigma'] = 0.05  # noise add into euler integration
-params['scale'] = 1 / 0.4
-
-Q = 0.01  # observation noise
+K = 1_000  # number of batches
+T = 300  # length of time series
 
 "different number of observations for sessions/animals"
-N = [30, 30, 30, 40, 50]
-
-"let's start with the same number of trials and trial length"
-K = 1_000  # number of batches
-T = 300  # length of trials
+dys = [30, 30, 40, 50]
 
 dx = 2
 t_eval = np.arange(0, (T+1) * dt, dt)
 
-x = np.empty((K, T, dx))
+
 
 data_all = []
 
-for j in range(len(N)):
-
-    data = {}
-
-    x0 = 6 * npr.rand(K, dx) - 3
-
-    C = np.random.randn(dx, N[j]) / np.sqrt(dx)
-    y = np.empty((K, T, N[j]))
-
-    for i in range(K):
-        x[i] = noisy_vanderpol(T+1, t_eval, params, x0[i])[1:]
-        y[i] = x[i]@C + np.expand_dims(np.random.randn(N[j]),0)*Q
-
+for dy in dys:
+    x, y, C = noisy_vanderpol_v2(K, T, dy, sigma_x, sigma_y, mu=mu, dt=dt)
+    data =  {}
     data['x'] = torch.from_numpy(x)
     data['y'] = torch.from_numpy(y)
     data['C'] = C
 
     data_all.append(data)
 
-data_path = 'data'
+# In[]
+fig, axs = plt.subplots(1, 2, figsize=(12, 6), dpi=100)
 
+for k in range(K):
+    axs[0].plot(data_all[0]['x'][k, :, 0], data_all[0]['x'][k, :, 1], alpha=0.3)
+    for d in range(30):
+        axs[1].plot(data_all[0]['y'][0, :, d], alpha=0.3)
+fig.show()
+
+
+# x = np.empty((K, T, dx))
+# for j in range(len(N)):
+#
+#     data = {}
+#
+#     x0 = 6 * npr.rand(K, dx) - 3
+#
+#     C = np.random.randn(dx, N[j]) / np.sqrt(dx)
+#     y = np.empty((K, T, N[j]))
+#
+#     for i in range(K):
+#         x[i] = noisy_vanderpol(T+1, t_eval, params, x0[i])[1:]
+#         y[i] = x[i]@C + np.expand_dims(np.random.randn(N[j]),0)*Q
+#
+#     data['x'] = torch.from_numpy(x)
+#     data['y'] = torch.from_numpy(y)
+#     data['C'] = C
+#
+#     data_all.append(data)
+
+data_path = 'data'
+print(torch.sum(torch.isnan(torch.from_numpy(y))))
 if not os.path.isdir(data_path):
     os.makedirs(data_path)
 
