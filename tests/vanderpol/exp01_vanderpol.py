41adf28e2aedccc91eb9d8ad4eb970c889d97717
Josue <josue.nassar@stonybrook.edu>
2023-06-30 16:18:35 -0400

    explicitly set shuffle to true. remove loading of x.

--- tests/vanderpol/exp01_vanderpol.py
+++ tests/vanderpol/exp01_vanderpol.py
@@ -24,19 +24,19 @@ data = torch.load('data/noisy_vanderpol.pt')
 def train_ref_vae():
     "extract reference data"
     # data is batch by time by dimension
-    x, y = data[0]['x'], data[0]['y']
+    y = data[0]['y'].float()
     dy_ref = y.shape[2]
-    N_train = int(np.ceil(0.8 * x.shape[0]))
-    x_train, y_train = x[:N_train].float(), y[:N_train].float()  # Batch by Time by Dimension
+    N_train = int(np.ceil(0.8 * y.shape[0]))
+    y_train = y[:N_train]  # Batch by Time by Dimension
 
     # Let's whiten the data to make it more numerically stable
     mu_train = torch.mean(y_train.reshape(-1, dy_ref), 0, keepdim=True)
     sigma_train = torch.std(y_train.reshape(-1, dy_ref), 0, keepdim=True)
     y_train = (y_train - mu_train) / sigma_train
 
-    data_ref = SeqDataLoader((y_train,), batch_size=100)
+    data_ref = SeqDataLoader((y_train,), batch_size=50, shuffle=True)
     vae = SeqVae(dx, dy_ref, dh, device=device)
-    res = vae_training(vae, data_ref)
+    res = vae_training(vae, data_ref, lr=5e-4, n_epochs=500)
 
     torch.save(res, 'trained_models/reference_model.pt')
 
@@ -61,8 +61,8 @@ def main():
     if not os.path.isdir(results_path):
         os.makedirs(results_path)
 
-    if not os.path.isfile(model_path + '/reference_model.pt'):
-        train_ref_vae()
+    # if not os.path.isfile(model_path + '/reference_model.pt'):
+    train_ref_vae()
 
     # res_alignment = reuse_dynamics(model_path + '/reference_model.pt', 50)
     #
