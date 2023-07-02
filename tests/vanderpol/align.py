00a45d24d308e4f8c09ddc76692c3ab2267db8a1
Josue <josue.nassar@stonybrook.edu>
2023-07-01 23:02:13 -0400

    working alignment code

--- tests/vanderpol/align.py
+++ tests/vanderpol/align.py
@@ -6,59 +6,65 @@ from seq_vae import SeqVae
 from tqdm import tqdm
 
 
-def compute_map_mse(ref_vae, linear_map, y):
+def compute_alignment_loss(ref_vae, linear_map, y, noisy_log_like=True):
     """
     loss for alignment between datasets
     ref_vae: pre-trained vae
-    prior: pre-trained prior
     linear_map: linear alignment matrix of size dy x dy_ref
-    y: new dataset to be aligned of shape K x T x dy TODO: shouldn't it be Time by K by dy?
+    y: new dataset to be aligned of shape K x T x dy
     """
     assert isinstance(ref_vae, SeqVae)
 
-    dy, dy_ref = linear_map.shape  # Assumption right now is that dy and dy_ref are of same dimension and no translations
+    dy, dy_ref = linear_map.shape  # assumption is that there is no translation
     y_tfm = y @ linear_map  # apply linear transformation to new dataset
 
-    x_samples, _, _, _ = ref_vae.encoder(y_tfm)  # for the given dataset
+    x_samples, _, _, log_q = ref_vae.encoder(y_tfm)  # for the given dataset
 
     # measure samples under the log prior to make sure it matches up with the learned generative model
     log_prior = ref_vae._prior(x_samples)
 
-    # now, we want to make sure we can reconstruct the original data, NOT y_tfm
-    # TODO: can we show that reconstructing y_tfm is equivalent to learning to reconstruct y???
-    mu_like_tfm, sigma_like_tfm = ref_vae.decoder.compute_param(x_samples)
+    # we first want to make sure we can reconstruct y_tfm
+    mu_like_tfm, var_like_tfm = ref_vae.decoder.compute_param(x_samples)
+    # log_like_tfm = torch.sum(Normal(mu_like_tfm, torch.sqrt(var_like_tfm)).log_prob(y_tfm), (-1, -2))
 
+    # now let's make sure we can reconstruct the original data
     # let's commit a sin and work with inverses
     inv_linear_map = torch.linalg.pinv(linear_map)
-    mu_like = mu_like_tfm @ inv_linear_map
-    sigma_like = inv_linear_map.T @ (sigma_like_tfm * torch.eye(dy_ref)) @ inv_linear_map
-    sigma_like = sigma_like + 1e-5 * torch.eye(dy)  # for numerical stability
-    log_like = torch.sum(MultivariateNormal(mu_like, covariance_matrix=sigma_like).log_prob(y))
 
-    loss = torch.mean(log_like + log_prior)
+    if noisy_log_like:
+        y_sample = mu_like_tfm + torch.sqrt(var_like_tfm) * torch.randn(mu_like_tfm.shape, device=mu_like_tfm.device)
+        log_like = torch.sum((y - y_sample) ** 2, (-1, -2))
+    else:
+        mu_like = mu_like_tfm @ inv_linear_map
+        sigma_like = inv_linear_map.T @ (var_like_tfm * torch.eye(dy_ref, device=y.device)) @ inv_linear_map
+        sigma_like = sigma_like + 1e-5 * torch.eye(dy, device=y.device)  # for numerical stability
+        log_like = torch.sum(MultivariateNormal(mu_like, covariance_matrix=sigma_like).log_prob(y), -1)
+
+    # loss = torch.mean(log_like + log_like_tfm + log_prior - log_q)
+    loss = torch.mean(log_like + log_prior - log_q)
     return -loss
 
 
-def train_invertible_mapping(ref_vae, y, dy_ref, n_epochs):
+def train_invertible_mapping(ref_vae, train_dataloader, dy_ref, n_epochs, noisy_log_like=True):
     """
     training function for learning linear alignment and updating prior params
     """
-    dy = y.shape[2]
-    linear_map = nn.Parameter(torch.randn(dy, dy_ref) / math.sqrt(dy), requires_grad=True)
+    dy = train_dataloader.data_tuple[0].shape[-1]
+    linear_map = nn.Parameter(torch.randn(dy, dy_ref, device=ref_vae.device) / math.sqrt(dy), requires_grad=True)
 
     training_losses = []
-    opt = torch.optim.Adam(params=[linear_map], lr=1e-3)
+    opt = torch.optim.AdamW(params=[linear_map], lr=1e-3, weight_decay=1e-4)
 
     for _ in tqdm(range(n_epochs)):
+        for y, in train_dataloader:
+            opt.zero_grad()
+            loss = compute_alignment_loss(ref_vae, linear_map, y.to(ref_vae.device),
+                                          noisy_log_like=noisy_log_like)
+            loss.backward()
+            opt.step()
 
-        # for y_b,  in data:
-        opt.zero_grad()
-        loss = compute_map_mse(ref_vae, linear_map, y)
-        loss.backward()
-        opt.step()
-
-        with torch.no_grad():
-            training_losses.append(loss.item())
+            with torch.no_grad():
+                training_losses.append(loss.item())
 
     return linear_map, training_losses
 
