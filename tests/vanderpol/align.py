76dca1beaea550bcea614363bf74fc13ef12fdac
ayesha vermani <ayesha@ayeshas-MBP.lan>
2023-07-10 00:56:15 +0100

    add likelihood for poisson observations

--- tests/vanderpol/align.py
+++ tests/vanderpol/align.py
@@ -1,6 +1,27 @@
 import torch.nn as nn
-from torch.distributions import Normal
+from torch.distributions import Normal, Poisson
 from utils import *
+
+
+def get_likelihood(obsv, f_dec, y, distribution='Normal'):
+
+    if distribution == "Normal":
+        mu_obsv, var_obsv = obsv
+        f_dec_mean, f_dec_var = f_dec
+
+        mu_obsv_tfm = f_dec_mean(mu_obsv)
+        var_obsv_tfm = torch.exp(f_dec_var(var_obsv))
+        log_like = torch.sum(Normal(loc=mu_obsv_tfm,
+                                    scale=torch.sqrt(var_obsv_tfm)).log_prob(y), (-1, -2))
+    elif distribution == "Poisson":
+        mu_obsv, var_obsv = obsv
+        f_dec_mean, bias = f_dec
+
+        y_sample = mu_obsv + torch.sqrt(var_obsv) * torch.randn(mu_obsv.shape, device=mu_obsv.device)
+
+        log_like = torch.sum(Poisson(torch.exp(f_dec_mean(y_sample) + bias)).log_prob(y), (-1,-2))
+
+    return log_like
 
 
 def compute_alignment_loss(ref_vae,
@@ -33,10 +54,8 @@ def compute_alignment_loss(ref_vae,
     mu_obsv, var_obsv = ref_vae.decoder.compute_param(x_samples)
 
     # transform parameters to new space using decoder
-    mu_obsv_tfm = f_dec_mean(mu_obsv)
-    var_obsv_tfm = torch.exp(f_dec_var(var_obsv))
-    log_like = torch.sum(Normal(loc=mu_obsv_tfm,
-                                scale=torch.sqrt(var_obsv_tfm)).log_prob(y), (-1, -2))
+    log_like = get_likelihood((mu_obsv, var_obsv), (f_dec_mean, f_dec_var), y, distribution='Poisson')
+
     loss = torch.mean(log_like + log_k_step_prior)
     return -loss
 
@@ -59,25 +78,26 @@ def train_invertible_mapping(ref_vae, train_dataloader, dy_ref,
     else:
         f_enc = nn.Linear(dy, dy_ref).to(ref_vae.device)
     f_dec_mean = nn.Linear(dy_ref, dy).to(ref_vae.device)
-    f_dec_var = nn.Linear(dy_ref, dy).to(ref_vae.device)
+    # f_dec_var = nn.Linear(dy_ref, dy).to(ref_vae.device)
+    bias = nn.Parameter(torch.rand(dy), requires_grad=True)
 
     training_losses = []
-    opt = torch.optim.AdamW(params=list(f_enc.parameters()) + list(f_dec_mean.parameters()) + list(f_dec_var.parameters()),
+    opt = torch.optim.AdamW(params=list(f_enc.parameters()) + list(f_dec_mean.parameters()) + [bias],
                             lr=5e-4, weight_decay=1e-4)
     torch.compile(ref_vae)
     for _ in tqdm(range(n_epochs)):
         for y, in train_dataloader:
             opt.zero_grad()
             loss = compute_alignment_loss(ref_vae, f_enc,
-                                          f_dec_mean, f_dec_var,
+                                          f_dec_mean, bias,
                                           y.to(ref_vae.device),
                                           K=K)
             loss.backward()
-            torch.nn.utils.clip_grad_norm_(list(f_enc.parameters()) + list(f_dec_mean.parameters()) + list(f_dec_var.parameters()),
+            torch.nn.utils.clip_grad_norm_(list(f_enc.parameters()) + list(f_dec_mean.parameters()) ,
                                            max_norm=1., norm_type=2)
             opt.step()
 
             with torch.no_grad():
                 training_losses.append(loss.item())
 
-    return (f_enc, f_dec_mean, f_dec_var), training_losses
+    return (f_enc, f_dec_mean, bias), training_losses
