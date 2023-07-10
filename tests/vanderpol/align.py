0c08aa222910b00dfc969d9dee3c02d8da959a4d
ayesha vermani <ayesha@ayeshas-mbp.champalimaud.pt>
2023-07-10 15:17:31 +0100

    reformat to include distribution argument

--- tests/vanderpol/align.py
+++ tests/vanderpol/align.py
@@ -3,11 +3,12 @@ from torch.distributions import Normal, Poisson
 from utils import *
 
 
-def get_likelihood(obsv, f_dec, y, distribution='Normal'):
+def get_likelihood(obsv, f_dec_params, y, distribution='Normal'):
+    "compute likelihood function based on distribution of new dataset"
 
     if distribution == "Normal":
         mu_obsv, var_obsv = obsv
-        f_dec_mean, f_dec_var = f_dec
+        f_dec_mean, f_dec_var = f_dec_params
 
         mu_obsv_tfm = f_dec_mean(mu_obsv)
         var_obsv_tfm = torch.exp(f_dec_var(var_obsv))
@@ -15,19 +16,19 @@ def get_likelihood(obsv, f_dec, y, distribution='Normal'):
                                     scale=torch.sqrt(var_obsv_tfm)).log_prob(y), (-1, -2))
     elif distribution == "Poisson":
         mu_obsv, var_obsv = obsv
-        f_dec_mean, bias = f_dec
+        f_dec_mean = f_dec_params
 
         y_sample = mu_obsv + torch.sqrt(var_obsv) * torch.randn(mu_obsv.shape, device=mu_obsv.device)
 
-        log_like = torch.sum(Poisson(torch.exp(f_dec_mean(y_sample) + bias)).log_prob(y), (-1,-2))
+        log_like = torch.sum(Poisson(torch.exp(f_dec_mean(y_sample) )).log_prob(y), (-1,-2))
 
     return log_like
 
 
 def compute_alignment_loss(ref_vae,
                            f_enc,
-                           f_dec_mean, f_dec_var,
-                           y, K):
+                           f_dec_params,
+                           y, K, distribution):
     """
     loss for alignment between datasets
     ref_vae: pre-trained vae
@@ -54,7 +55,7 @@ def compute_alignment_loss(ref_vae,
     mu_obsv, var_obsv = ref_vae.decoder.compute_param(x_samples)
 
     # transform parameters to new space using decoder
-    log_like = get_likelihood((mu_obsv, var_obsv), (f_dec_mean, f_dec_var), y, distribution='Poisson')
+    log_like = get_likelihood((mu_obsv, var_obsv), f_dec_params, y, distribution=distribution)
 
     loss = torch.mean(log_like + log_k_step_prior)
     return -loss
@@ -63,6 +64,8 @@ def compute_alignment_loss(ref_vae,
 def train_invertible_mapping(ref_vae, train_dataloader, dy_ref,
                              n_epochs,
                              K=40,
+                             lr=1e-3,
+                             distribution='Normal',
                              linear_flag=False):
     """
     training function for learning linear alignment and updating prior params
@@ -77,27 +80,38 @@ def train_invertible_mapping(ref_vae, train_dataloader, dy_ref,
                                 nn.Linear(128, dy_ref)]).to(ref_vae.device)
     else:
         f_enc = nn.Linear(dy, dy_ref).to(ref_vae.device)
+
     f_dec_mean = nn.Linear(dy_ref, dy).to(ref_vae.device)
-    # f_dec_var = nn.Linear(dy_ref, dy).to(ref_vae.device)
-    bias = nn.Parameter(torch.rand(dy), requires_grad=True)
+
+    if distribution == 'Normal':
+
+        f_dec_var = nn.Linear(dy_ref, dy).to(ref_vae.device)
+
+        f_dec_params = (f_dec_mean, f_dec_var)
+        f_dec_params_list = list(f_dec_mean.parameters()) + list(f_dec_var.parameters())
+
+    elif distribution == 'Poisson':
+
+        f_dec_params = f_dec_mean
+        f_dec_params_list = list(f_dec_mean.parameters())
 
     training_losses = []
-    opt = torch.optim.AdamW(params=list(f_enc.parameters()) + list(f_dec_mean.parameters()) + [bias],
-                            lr=5e-4, weight_decay=1e-4)
+    opt = torch.optim.AdamW(params=list(f_enc.parameters()) + f_dec_params_list,
+                            lr=lr, weight_decay=1e-4)
     torch.compile(ref_vae)
     for _ in tqdm(range(n_epochs)):
         for y, in train_dataloader:
             opt.zero_grad()
             loss = compute_alignment_loss(ref_vae, f_enc,
-                                          f_dec_mean, bias,
+                                          f_dec_params,
                                           y.to(ref_vae.device),
-                                          K=K)
+                                          K=K, distribution=distribution)
             loss.backward()
-            torch.nn.utils.clip_grad_norm_(list(f_enc.parameters()) + list(f_dec_mean.parameters()) ,
+            torch.nn.utils.clip_grad_norm_(list(f_enc.parameters()) + f_dec_params_list,
                                            max_norm=1., norm_type=2)
             opt.step()
 
             with torch.no_grad():
                 training_losses.append(loss.item())
 
-    return (f_enc, f_dec_mean, bias), training_losses
+    return (f_enc, f_dec_params), training_losses
