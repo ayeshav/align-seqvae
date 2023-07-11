519562055a8277a7b4219fd0457ca3b9335c03fb
ayesha vermani <ayesha@ayeshas-MacBook-Pro.local>
2023-07-11 23:30:25 +0100

    experiment code to include option to include mlp

--- tests/vanderpol/exp01_vanderpol.py
+++ tests/vanderpol/exp01_vanderpol.py
@@ -19,10 +19,10 @@ device = 'cpu'
 print(device)
 
 "load vanderpol data"
-data = torch.load('data/noisy_vanderpol.pt')
+data = torch.load('data/noisy_vanderpol_bernoulli.pt')
 
 
-def train_ref_vae():
+def train_ref_vae(normalize=True, featurize=False):
     "extract reference data"
     # data is batch by time by dimension
     y = data[0]['y'].float()
@@ -31,15 +31,21 @@ def train_ref_vae():
     y_train = y[:N_train]  # Batch by Time by Dimension
 
     # Let's whiten the data to make it more numerically stable
-    mu_train = torch.mean(y_train.reshape(-1, dy_ref), 0, keepdim=True)
-    sigma_train = torch.std(y_train.reshape(-1, dy_ref), 0, keepdim=True)
-    y_train = (y_train - mu_train) / sigma_train
+    if normalize:
+        mu_train = torch.mean(y_train.reshape(-1, dy_ref), 0, keepdim=True)
+        sigma_train = torch.std(y_train.reshape(-1, dy_ref), 0, keepdim=True)
+        y_train = (y_train - mu_train) / sigma_train
+
+    if featurize:
+        net = Mlp(dy_ref, 64, 128, device=device)
+    else:
+        net = None
 
     data_ref = SeqDataLoader((y_train,), batch_size=100, shuffle=True)
-    vae = SeqVae(dx, dy_ref, dh, device=device)
-    res = vae_training(vae, data_ref, lr=1e-3, n_epochs=2_000)
+    vae = SeqVae(dx, 64, dh, dy_ref, likelihood='Bernoulli', device=device)
+    res = vae_training(vae, data_ref, inp_tfm=net, lr=1e-3, n_epochs=2_000)
 
-    torch.save(res, 'trained_models/reference_model.pt')
+    torch.save(res, 'trained_models/reference_model_bernoulli.pt')
 
 
 def reuse_dynamics(reference_model, epochs=20):
@@ -52,16 +58,16 @@ def reuse_dynamics(reference_model, epochs=20):
 
     dy_ref = data[0]['y'].float().shape[2]
 
-    with torch.no_grad():
-        x_samples, mu, var, _ = vae.encoder(data[0]['y'].float())
-
-        mu_ref = torch.mean(x_samples.reshape(x_samples.shape[-1], -1), 1, keepdim=True)
-        cov_ref = torch.cov(x_samples.reshape(x_samples.shape[-1], -1))
+    # with torch.no_grad():
+    #     x_samples, mu, var, _ = vae.encoder(data[0]['y'].float())
+    #
+    #     mu_ref = torch.mean(x_samples.reshape(x_samples.shape[-1], -1), 1, keepdim=True)
+    #     cov_ref = torch.cov(x_samples.reshape(x_samples.shape[-1], -1))
 
     # for i in range(1, len(data)):
     y_dataloader = SeqDataLoader((y_train,), batch_size=100, shuffle=True)
-    res_alignment = train_invertible_mapping(vae, y_dataloader, dy_ref, mu_ref,
-                                             cov_ref, epochs, beta=50)
+    res_alignment = train_invertible_mapping(vae, y_dataloader, dy_ref, epochs,
+                                             distribution='Bernoulli')
 
     return res_alignment
 
@@ -76,10 +82,10 @@ def main():
     if not os.path.isdir(results_path):
         os.makedirs(results_path)
 
-    if not os.path.isfile(model_path + '/reference_model.pt'):
-        train_ref_vae()
+    if not os.path.isfile(model_path + '/reference_model_bernoulli.pt'):
+        train_ref_vae(normalize=False, featurize=True)
 
-    res_alignment = reuse_dynamics(model_path + '/reference_model.pt', 150)
+    res_alignment = reuse_dynamics(model_path + '/reference_model_bernoulli.pt', 150)
     torch.save(res_alignment, 'result_prior.pt')
 
 
