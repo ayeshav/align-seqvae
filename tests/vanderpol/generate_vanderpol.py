a85fdb9677207972b483243f9ec029fcdcbfefac
Josue <josue.nassar@stonybrook.edu>
2023-07-11 11:12:29 -0400

    delete old function in generate_vanderpol to make it easier to modify

--- tests/vanderpol/generate_vanderpol.py
+++ tests/vanderpol/generate_vanderpol.py
@@ -10,44 +10,6 @@ torch.random.manual_seed(42)
 npr.seed(0)
 
 
-def vanderpol_osc(T, t_eval, mu, x0):
-    "function to generate noiseless vanderpol oscillator"
-
-    def f(state,t):
-        x, y = state
-
-        xdot = y
-        ydot = -mu*(x**2-1)*y - x
-
-        return xdot, ydot
-
-    result = odeint(f, x0, t_eval)
-
-    return result
-
-
-def noisy_vanderpol(T, t_eval, params, x0):
-    dt = t_eval[1] - t_eval[0]
-    mu = params['mu']
-    tau_1 = params['tau_1']
-    tau_2 = params['tau_2']
-    sigma = params['sigma']
-    scale = params['scale']
-
-    x = np.zeros((T, 2))
-    x[0, 0] = x0[0]
-    x[0, 1] = x0[1]
-
-    for dx in range(1, T):
-        x_next = x[dx - 1, 0] + (1 / scale) * (dt / tau_1) * scale * x[dx - 1, 1]
-        y_next = x[dx - 1, 1] + (1 / scale) * (dt / tau_2) * (mu * (1 - scale**2 * x[dx - 1, 0]**2) * scale * x[dx - 1, 1] - scale * x[dx - 1, 0])
-
-        x[dx, 0] = x_next + (sigma / scale) * np.random.randn()
-        x[dx, 1] = y_next + (sigma / scale) * np.random.randn()
-
-    return x
-
-
 def noisy_vanderpol_v2(K, T, dy, sigma_x, sigma_y, mu=1.5, dt=1e-2, noise_type='poisson'):
     x = np.empty((K, T, 2))
     y = torch.empty((K, T, dy))
@@ -81,19 +43,6 @@ def noisy_vanderpol_v2(K, T, dy, sigma_x, sigma_y, mu=1.5, dt=1e-2, noise_type='
         elif noise_type == 'poisson':
             y[:, t] = torch.poisson(dt * torch.exp(torch.from_numpy(x[:, t]) @ C.T + b.unsqueeze(0)))
     return x, y, C, b
-
-
-# "params for vdp"
-# mu = 1.5
-# dt = 1e-2
-#
-# "params dict for noisy vdp"
-# params = {}
-# params['mu'] = mu
-# params['tau_1'] = 0.1
-# params['tau_2'] = 0.1
-# params['sigma'] = 0.05  # noise add into euler integration
-# params['scale'] = 1 / 0.4
 
 sigma_x = 0.5  # state noise
 sigma_y = 0.1  # observation noise
