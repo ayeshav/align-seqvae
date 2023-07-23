a8ea50e8d5db1a40a99951abc207ee8d225b0fcc
Josue <josue.nassar@stonybrook.edu>
2023-07-23 12:39:15 -0400

    add coordinate ascent

--- tests/vanderpol/dev_test/train.py
+++ tests/vanderpol/dev_test/train.py
@@ -5,7 +5,7 @@ from utils import *
 
 
 def dualvae_training(vae, train_dataloader, n_epochs=100, lr=5e-4,
-                 weight_decay=1e-4):
+                 weight_decay=1e-4, reg_weight=100):
     """
     function that will train a vae
     :param vae: a SeqVae object
@@ -26,7 +26,46 @@ def dualvae_training(vae, train_dataloader, n_epochs=100, lr=5e-4,
     for _ in tqdm(range(n_epochs)):
         for y, y_other in train_dataloader:
             opt.zero_grad()
-            loss = vae(y.to(vae.device), y_other.to(vae.device))
+            loss = vae(y.to(vae.device), y_other.to(vae.device), reg_weight=reg_weight)
+            loss.backward()
+            opt.step()
+
+            with torch.no_grad():
+                training_losses.append(loss.item())
+    return vae, training_losses
+
+
+def dualvae_coordinate_ascent_training(vae, train_dataloader, n_epochs=100, lr=5e-4,
+                                       weight_decay=1e-4, reg_weight=100):
+    """
+    function that will train a vae
+    :param vae: a SeqVae object
+    :param train_dataloader: a dataloader object
+    :param n_epochs: Number of epochs to train for
+    :param lr: learning rate of optimizer
+    :param weight_decay: value of weight decay
+    :return: trained vae and training losses
+    """
+    assert isinstance(vae, DualAnimalSeqVae)
+    assert train_dataloader.shuffle
+    assert isinstance(train_dataloader, SeqDataLoader)
+
+    training_losses = []
+
+    train_other_animal = True
+    for j in tqdm(range(n_epochs)):
+        if train_other_animal:
+            param_list = list(vae.f_enc.parameters()) + list(vae.other_animal_decoder.parameters())
+            train_other_animal = False
+        else:
+            param_list = list(vae.encoder.parameters()) + list(vae.prior.parameters()) + list(vae.decoder.parameters())
+            train_other_animal = True
+
+        opt = torch.optim.AdamW(param_list, lr=lr, weight_decay=weight_decay)
+
+        for y, y_other in train_dataloader:
+            opt.zero_grad()
+            loss = vae(y.to(vae.device), y_other.to(vae.device), reg_weight=reg_weight)
             loss.backward()
             opt.step()
 
