430962b6ad00360dc244b260ed433b9c8910657c
ayesha <ayeshavermani@gmail.com>
2023-10-07 18:06:56 +0100

    conditional vae inherits fns from seqvae

--- src/seq_vae.py
+++ src/seq_vae.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.distributions import Normal
-from utils import compute_wasserstein, vectorize_x
+from utils import compute_wasserstein, vectorize
 
 Softplus = torch.nn.Softplus()
 eps = 1e-6
@@ -17,68 +17,17 @@ class SeqVae(nn.Module):
         self.decoder = decoder
         self.device = device
 
-    def _prior(self, x_samples):
-        """
-        Compute log p(x_t | x_{t-1})
-        :param x_samples: A tensor of latent samples of dimension Batch by Time by Dy
-        :return:
-        """
-        if self.k_step == 1:
-            log_prior = torch.sum(Normal(torch.zeros(1, device=self.device),
-                                         torch.ones(1, device=self.device)).log_prob(x_samples[:, 0]), -1)
-            log_prior = log_prior + self.prior(x_samples[:, :-1], x_samples[:, 1:])
-        else:
-            log_prior = 0
+    def compute_k_step_log_q(self, y):
 
-            for t in range(x_samples.shape[1] - 1):
-                K_ahead = min(self.k_step, x_samples[:, t + 1:].shape[1])
-                _, mu_k_ahead, var_k_ahead = self.prior.sample_k_step_ahead(x_samples[:, t], K_ahead)
-                log_prior = log_prior + torch.sum(
-                    Normal(mu_k_ahead, torch.sqrt(var_k_ahead)).log_prob(x_samples[:, t + K_ahead]), -1)
-        return log_prior
-
-    def forward(self, y, beta=1.):
-        """
-        In the forward method, we compute the negative elbo and return it back
-        :param y: Y is a tensor of observations of size Batch by Time by Dy
-        :return:
-        """
-        # pass data through encoder and get mean, variance, samples and log density
-        x_samples, mu, var, log_q = self.encoder(y)
-
-        # given samples, compute the log prior
-        log_prior = self._prior(x_samples)
-
-        # given samples, compute the log likelihood
-        log_like = self.decoder(x_samples, y)
-
-        # compute the elbo
-        elbo = torch.mean(log_like + beta * (log_prior - log_q))
-        return -elbo
-
-
-class CondSeqVae(nn.Module):
-    def __init__(self, prior, encoder, decoder, readout_behav=None, device='cpu', k_step=1):
-        super().__init__()
-        self.k_step = k_step
-
-        self.prior = prior
-        self.encoder = encoder
-        self.decoder = decoder
-        self.readout_behav = readout_behav
-
-        self.device = device
-
-    def _compute_k_step_log_q(self, y):
         x_samples, mu, var = self.encoder.sample(y)
 
         if self.k_step == 1:
             log_q = torch.sum(Normal(mu, torch.sqrt(var)).log_prob(x_samples), (-2, -1))
         else:
 
-            x_k = vectorize_x(x_samples[:, 1:], self.k_step)
-            mu_k = vectorize_x(mu[:, 1:], self.k_step)
-            var_k_step = vectorize_x(var[:, 1:], self.k_step)
+            x_k = vectorize(x_samples[:, 1:], self.k_step)
+            mu_k = vectorize(mu[:, 1:], self.k_step)
+            var_k_step = vectorize(var[:, 1:], self.k_step)
 
             "mean across k steps"
             log_q = torch.mean(Normal(mu_k, torch.sqrt(var_k_step)).log_prob(x_k), -2)
@@ -94,47 +43,87 @@ class CondSeqVae(nn.Module):
 
         return x_samples, mu, var, log_q
 
-    def _compute_k_step_log_prior(self, x_samples, u):
+    def compute_k_step_log_prior(self, x_samples):
         """
         function to compute multi-step prediction (1/K) * sum_{i=1:K} p(x_{t+k} | x_t)
         """
-        dx = x_samples.shape[-1]
-        du = u.shape[-1]
-
-        x_prev = torch.cat((x_samples[:, :-self.k_step], u[:, :-self.k_step]), axis=-1)
+        dx = self.prior.dx
+        x_prev = x_samples[:, :-1]
 
         if self.k_step == 1:
             log_k_step_prior = self.prior(x_prev, x_samples[..., 1:, :])
-
         else:
             # get vectorized k-step windows of x_samples BW x k_step x dx
-            x_k = vectorize_x(x_samples[..., 1:, :], self.k_step)
-            u_k = vectorize_x(u[..., 1:, :], self.k_step)
+            x_k = vectorize(x_samples[..., 1:, :], self.k_step)
 
-            _, mu_k_ahead, var_k_ahead = self.prior.sample_k_step_ahead(x_prev.reshape(-1, 1, dx + du),
-                                                                        self.k_step, u=u_k,
+            _, mu_k_ahead, var_k_ahead = self.prior.sample_k_step_ahead(x_prev[:, : -(self.k_step+1)].reshape(-1, 1, x_prev.shape[-1]),
+                                                                        self.k_step, u=x_k[..., dx:],
                                                                         keep_trajectory=True)
-            # take mean along k-steps
-            log_k_step_prior = torch.mean(Normal(mu_k_ahead, torch.sqrt(var_k_ahead)).log_prob(x_k), -2)
-
-            # sum along time and dx
+            #
+            log_k_step_prior = torch.mean(Normal(mu_k_ahead, torch.sqrt(var_k_ahead)).log_prob(x_k[..., :dx]), -2)
             log_k_step_prior = torch.sum(log_k_step_prior.reshape(x_samples.shape[-3], -1, dx), (-1, -2))
 
             # compute prediction for the last k-1 steps
             for i in torch.arange(self.k_step - 1):
                 K_ahead = self.k_step - i - 1
 
-                x_prev = torch.cat((x_samples[..., -(K_ahead + 1), :], u[..., -(-K_ahead + 1), :]), axis=-1)
-                _, mu_k, var_k = self.prior.sample_k_step_ahead(x_prev.unsqueeze(1), K_ahead, u=u[..., -K_ahead:, :],
-                                                                keep_trajectory=True)
-                log_prior = torch.sum(
-                    Normal(mu_k, torch.sqrt(var_k)).log_prob(x_samples[:, -K_ahead:].reshape(-1, K_ahead, dx)),
-                    (-1, -2))
-                log_k_step_prior = log_k_step_prior + log_prior / K_ahead
+                _, mu_k, var_k = self.prior.sample_k_step_ahead(x_prev[..., -(K_ahead + 1), :].unsqueeze(1), K_ahead,
+                                                                u=x_samples[..., -K_ahead:, dx:], keep_trajectory=True)
+
+                log_prior = Normal(mu_k, torch.sqrt(var_k)).log_prob(x_samples[:, -K_ahead:].reshape(-1, K_ahead, dx))
+                log_k_step_prior = log_k_step_prior + torch.sum(log_prior, (-1, -2))/K_ahead
 
         return log_k_step_prior
 
-    def forward(self, y, u, y_behav=None, beta=1.):
+    # def _prior(self, x_samples):
+    #     """
+    #     Compute log p(x_t | x_{t-1})
+    #     :param x_samples: A tensor of latent samples of dimension Batch by Time by Dy
+    #     :return:
+    #     """
+    #     if self.k_step == 1:
+    #         log_prior = torch.sum(Normal(torch.zeros(1, device=self.device),
+    #                                      torch.ones(1, device=self.device)).log_prob(x_samples[:, 0]), -1)
+    #         log_prior = log_prior + self.prior(x_samples[:, :-1], x_samples[:, 1:])
+    #     else:
+    #         log_prior = 0
+    #
+    #         for t in range(x_samples.shape[1] - 1):
+    #             K_ahead = min(self.k_step, x_samples[:, t + 1:].shape[1])
+    #             _, mu_k_ahead, var_k_ahead = self.prior.sample_k_step_ahead(x_samples[:, t], K_ahead)
+    #             log_prior = log_prior + torch.sum(
+    #                 Normal(mu_k_ahead, torch.sqrt(var_k_ahead)).log_prob(x_samples[:, t + K_ahead]), -1)
+    #     return log_prior
+
+    def forward(self, y, beta=1.):
+        """
+        In the forward method, we compute the negative elbo and return it back
+        :param y: Y is a tensor of observations of size Batch by Time by Dy
+        :return:
+        """
+        # pass data through encoder and get mean, variance, samples and log density
+        x_samples, _, _, log_q = self.compute_k_step_log_q(y)
+
+        # given samples, compute the log prior
+        log_prior = self.compute_k_step_log_prior(x_samples)
+
+        # given samples, compute the log likelihood
+        log_like = self.decoder(x_samples, y)
+
+        # compute the elbo
+        elbo = torch.mean(log_like + beta * (log_prior - log_q))
+        return -elbo
+
+
+class CondSeqVae(SeqVae):
+    def __init__(self, prior, encoder, decoder, readout_behav=None, device='cpu', k_step=1):
+        super().__init__(prior=prior, encoder=encoder, decoder=decoder, device=device)
+        self.k_step = k_step
+
+        self.readout_behav = readout_behav
+        self.device = device
+
+    def forward(self, batch, beta=1.):
         """
         In the forward method, we compute the negative elbo and return it back
         :param y: Y is a tensor of observations of size Batch by Time by Dy
@@ -142,25 +131,24 @@ class CondSeqVae(nn.Module):
         :param y_behav: tensor of behavior (example, hand velocity) of size B x T x Y_behav
         :return:
         """
+        if len(batch) > 2:
+            y, u, y_behav = batch
+        else:
+            y, u = batch
+
         # pass data through encoder and get mean, variance, samples and log density
-        x_samples, mu, var, log_q = self._compute_k_step_log_q(torch.cat((y, u), -1))
-
-        log_prior_0 = torch.sum(Normal(torch.zeros(1, device=self.device),
-                                       0.5 * torch.ones(1, device=self.device)).log_prob(x_samples[:, 0]), -1)
-        log_q_0 = torch.sum(Normal(mu[:, 0], torch.sqrt(var[:, 0])).log_prob(x_samples[:, 0]), -1)
-
-        kl_0 = log_prior_0 - log_q_0
+        x_samples, _, _, log_q = self.compute_k_step_log_q(torch.cat((y, u), -1))
 
         # given samples, compute the log prior
-        log_prior = self._compute_k_step_log_prior(x_samples, u)
+        log_prior = self.compute_k_step_log_prior(torch.cat((x_samples, u), -1))
 
         # given samples, compute the log likelihood
         log_like = self.decoder(x_samples, y)
 
         # compute the elbo
-        elbo = torch.mean(log_like + beta * (kl_0 + log_prior - log_q))
+        elbo = torch.mean(log_like + beta * (log_prior - log_q))
 
-        if y_behav is not None:
+        if len(batch) > 2:
             log_like_behav = self.readout_behav(x_samples, y_behav)
 
             return -elbo - torch.mean(log_like_behav)
